a2be3a7657846128e65226b40a4af56a457c9a10
Mandi Walls <mandi.walls@gmail.com>
2014-10-29 15:40:31 -0400

    still fighting with mod_wsgi

--- cookbooks/aar/recipes/httpd.rb
+++ cookbooks/aar/recipes/httpd.rb
@@ -1,6 +1,10 @@
 httpd_service 'aar' do
-  modules ['mod_wsgi']
   action :create
+end
+
+httpd_module 'mod_wsgi' do
+  instance 'aar'
+  notifies :restart, 'httpd_service[aar]'
 end
 
 httpd_config 'aar' do
