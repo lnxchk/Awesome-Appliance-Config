700a24f4570841da529659264d301f9177787dd3
Mandi Walls <mandi.walls@gmail.com>
2014-10-29 22:06:03 -0400

    persisting db secrets

--- cookbooks/aar/recipes/aar.rb
+++ cookbooks/aar/recipes/aar.rb
@@ -3,14 +3,14 @@
 # /var/www/AAR/AAR_config.py
 
 require 'securerandom'
-db_passwd = SecureRandom.urlsafe_base64(6)
-secret_key = SecureRandom.urlsafe_base64(12)
+node.set_unless['aar']['db_passwd'] = SecureRandom.urlsafe_base64(6)
+node.set_unless['aar']['secret_key'] = SecureRandom.urlsafe_base64(12)
 
 template "/var/www/AAR/AAR_config.py" do
   source "aar_config.erb"
   variables(
-    :passwd => db_passwd,
-    :secret => secret_key
+    :passwd => node['aar']['db_passwd'],
+    :secret => node['aar']['secret_key']
   )
   owner node['aar']['system_user']
   group node['aar']['system_group']
