fbbe39c2418ba08fe83312b6fbb18540bb0018d0
Mandi Walls <mandi.walls@gmail.com>
2014-10-29 21:58:39 -0400

    hash rockets

--- cookbooks/aar/recipes/aar.rb
+++ cookbooks/aar/recipes/aar.rb
@@ -9,8 +9,8 @@ secret_key = SecureRandom.urlsafe_base64(12)
 template "/var/www/AAR/AAR_config.py" do
   source "aar_config.erb"
   variables(
-    :passwd = db_passwd,
-    :secret = secret_key
+    :passwd => db_passwd,
+    :secret => secret_key
   )
   owner node['aar']['system_user']
   group node['aar']['system_group']
