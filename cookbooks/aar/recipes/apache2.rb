6b1610eb0b7b4074abfe4b9c9b699f75627455de
Mandi Walls <mandi.walls@gmail.com>
2014-11-04 10:59:45 +0000

    using link resource instead of the debian tools

--- cookbooks/aar/recipes/apache2.rb
+++ cookbooks/aar/recipes/apache2.rb
@@ -8,7 +8,19 @@ service "apache2" do
   action [ :start, :enable ]
 end
 
-template "/etc/apache2/sites-enabled/aar.conf" do
+template "/etc/apache2/sites-available/aar.conf" do
   source "aar.erb"
   notifies :restart, "service[apache2]"
 end
+
+link "/etc/apache2/sites-enabled/aar.conf" do
+  to "/etc/apache2/sites-available/aar.conf" 
+  notifies :restart, "service[apache2]"
+end
+
+link "/etc/apache2/sites-enabled/000-default" do
+  to "/etc/apache2/sites-available/000-default"
+  action :delete
+  notifies :restart, "service[apache2]"
+end
+
