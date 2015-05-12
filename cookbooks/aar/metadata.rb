242da44899ab1318d42cb89089965e5c41f6a2d4
Mandi Walls <mandi.walls@gmail.com>
2015-05-12 14:30:34 +0100

    re-init'ing test kitchen

--- cookbooks/aar/metadata.rb
+++ cookbooks/aar/metadata.rb
@@ -6,8 +6,3 @@ description      'Installs/Configures aar'
 long_description 'Installs/Configures aar'
 version          '0.1.0'
 
-depends 'apt'
-depends 'ark'
-depends 'python'
-depends 'database'
-depends 'mysql2_chef_gem'
